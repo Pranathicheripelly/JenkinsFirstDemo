e76956c65b14457c70ad7cd74dd8610f3885d735
Pranathicheripelly <Cheripellypranathi123@gmail.com>
2023-12-04 21:01:30 +0530

    second commit

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel Model = new ProductModel();
+            Model.ProductID = 100;
+            Model.ProductName = "Nani";
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class ProductModel
+    {
+        public int ProductID {  get; set; }
+    }
+}
